c548ccfd1eaf0217e4ee8135a3667e23f4525cc4
Junny <anh.ntl.hust@gmail.com>
2025-06-12 10:56:53 +0700

    Add Chapter-12/Python and update DA

---
+++ Python/Chapter-12/settings.py
@@ -0,0 +1,17 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+    
+    def __init__(self):
+        """Initialize the game's settings."""
+        #screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
+        #Bullet setting
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 16
+        self.bullet_color = (60, 60, 60)
+        self.bullet_allowed = 3
+        
