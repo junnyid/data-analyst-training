b1a0fce7492d4ccaf54992b5d7add24a89235649
Junny <anh.ntl.hust@gmail.com>
2025-06-15 18:36:59 +0700

    Add Chapter 15

---
+++ Python/Chapter-15/mpl_squares.py
@@ -0,0 +1,19 @@
+from cProfile import label
+import matplotlib.pyplot as plt
+
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(input_values, squares, linewidth=3)
+
+# Set chart title and label  axes
+ax.set_title("Square Number", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+#Set size of tick label
+ax.tick_params(labelsize=14)
+
+plt.show()
