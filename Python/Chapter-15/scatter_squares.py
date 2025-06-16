3ed52243de8add49e50962435e0010148334d799
Junny <anh.ntl.hust@gmail.com>
2025-06-16 15:28:09 +0700

    Update Chapter 15

--- Python/Chapter-15/scatter_squares.py
+++ Python/Chapter-15/scatter_squares.py
@@ -5,7 +5,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-v0_8')
 fix, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Greens, s=10)
 
 #Set chart title and label axes
 ax.set_title("Square Numbers", fontsize=24)
@@ -19,4 +19,5 @@ ax.tick_params(labelsize=14)
 ax.axis([0, 1100, 0, 1_100_000])
 ax.ticklabel_format(style='plain')
 
+plt.savefig('squares_plot.png', bbox_inches='tight')
 plt.show()
