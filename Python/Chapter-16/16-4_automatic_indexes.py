5b832aa6ca431e35ffc531d064956e2071ace58d
Junny <anh.ntl.hust@gmail.com>
2025-06-17 17:04:22 +0700

    The CSV files

---
+++ Python/Chapter-16/16-4_automatic_indexes.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+from matplotlib.rcsetup import ValidateInStrings
+from scipy.datasets import face
+
+path = Path('Python/Chapter-16/weather_data/death_valley_2021_simple.csv')
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+date_index = header_row.index('DATE')
+high_index = header_row.index('TMAX')
+low_index = header_row.index('TMIN')
+name_index = header_row.index('NAME')
+
+#Extract dates, highs, low temperature
+dates, highs, lows = [], [], []
+places = []
+for row in reader:
+    if not places:
+        places = row[name_index]
+    current_date = datetime.strptime(row[date_index], '%Y-%m-%d')
+    try:
+        high = int(row[high_index])
+        low = int(row[low_index])
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+#Plot weather data for Sitka
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, color='red', alpha=0.5)
+ax.plot(dates, lows, color='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+#Format plot
+title = f"Daily High and Low Temperatures, 2021\n{places}"
+ax.set_title(title, fontsize=20)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=14)
+ax.tick_params(labelsize=14)
+
+plt.show()
--- Python/Chapter-16/16-5_explore_vietnam.py
+++ Python/Chapter-16/16-5_explore_vietnam.py
@@ -4,33 +4,47 @@ from datetime import datetime
 import csv
 from turtle import title
 import matplotlib.pyplot as plt
+from numpy import place
 
-path = Path('Python/Chapter-16/weather_data/san_francisco_weather_data.csv')
+path = Path('Python/Chapter-16/weather_data/vietnam_weather_summary.csv')
 lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
 
+date_index = header_row.index('date')
+high_index = header_row.index('tmax')
+low_index = header_row.index('tmin')
+name_index = header_row.index('name')
+
 #Extract dates, highs ans lows temperatures.
 dates, highs, lows = [], [], []
+places = []
 for row in reader:
+    if not places:
+        places = row[name_index]
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
-    high = float(row[3])
-    low = float(row[4])
-    dates.append(current_date)
-    highs.append(high)
-    lows.append(low)
+    try:
+        high = float(row[high_index])
+        low = float(row[low_index])
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+        
 #Plot the high and low temperatures.
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, color='red', alpha=0.5)
 ax.plot(dates, lows, color='blue', alpha=0.5)
 #Format plot
-title = "Daily High and Low Temperatures, 2025\nSan Francisco"
+title = "Daily High and Low Temperatures, 2025\nViet Nam"
 ax.set_title(title, fontsize=20)
 ax.set_xlabel('', fontsize=14)
 fig.autofmt_xdate()
-ax.set_ylabel("Temperature (F)", fontsize=14)
+ax.set_ylabel("Temperature (C)", fontsize=14)
 ax.tick_params(labelsize=14)
 
 plt.show()
